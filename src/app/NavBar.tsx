4f67f2c1fab6e35811113f0b9ebeb09495379391
AbhayShaw0301 <abhayshaw0301@gmail.com>
2024-03-21 23:18:01 +0530

    setting up the project and adding NavBar

---
+++ src/app/NavBar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import {Container, Nav, Navbar} from "react-bootstrap";
+import Link from "next/link" ;
+import {usePathname} from "next/navigation";
+
+export default function NavBar(){
+    const pathName=usePathname();
+    return <Navbar bg="secondary" variant="dark"  sticky="top" expand="sm" collapseOnSelect>
+        <Container>
+            <Navbar.Brand as={Link}  href="/">
+                   GalleryPod
+            </Navbar.Brand>
+            <Navbar.Toggle  aria-controls="main-navbar" />
+            <Navbar.Collapse id="main-navbar">
+            <Nav>
+              <Nav.Link as={Link} href="/hello" active={pathName==="/hello"}>Hello World</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import {Spinner} from "@/components/bootsrap";
+export default function loading(){
+    return <Spinner animation="border" className="d-block m-auto"/>
+
+}
