dc5f8c06027afef7722d98d5f7cf4ceeced6fab8
AbhayShaw0301 <abhayshaw0301@gmail.com>
2024-03-25 19:58:02 -0700

    fixed dynamic page typo issue

--- src/app/dynamic/page.tsx
+++ src/app/dynamic/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default async function UnsplashImage() {
     const response= await fetch('https://api.unsplash.com/photos/random/?client_id='+process.env.UNSPLASH_ACCESS_KEY,{
-        next:{revalidate:10}
+        next:{revalidate:0}
     });
     const image:UnsplashImg = await response.json();
     const width =Math.min(500,image.width);
@@ -18,7 +18,7 @@ export default async function UnsplashImage() {
   return (
     <div className="d-flex flex-column align-items-center">
             <Alert>
-            This page <strong>fetches data dynamically.</strong> Ever time you refresh the page after 10s you get a new Image.
+            This page <strong>fetches data dynamically.</strong> Ever time you refresh the page  you get a new Image.
             </Alert>
             <Image src={image.urls.raw} alt={image.description} width={width} height={height} className="rounded shadow mw-100 h-100"/>
             
